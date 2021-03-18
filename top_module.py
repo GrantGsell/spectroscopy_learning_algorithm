638f267e6d080389806f58c7eb06f5b569ecd714
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2021-03-17 23:45:41 -0400

    Updated for testing

--- top_module.py
+++ top_module.py
@@ -1,16 +1,15 @@
 from new_image_prediction import *
 from learning_algorithm import *
-from data_creation import *
+from data_collection import *
 
 
 def main():
     obj_learn_algo = LearningAlgorithm()
     obj_learn_algo.top_learning_algorithm()
 
-    num_classes = obj_learn_algo.num_classes
-    obj_new_img_pred = ImagePrediction(num_classes, False)
+    obj_new_img_pred = ImagePrediction(False)
     obj_new_img_pred.top_image_prediction('new_image_test.jpg')
-
+    print("Done")
     return
 
 if __name__ == "__main__":
